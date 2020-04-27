0112bcae997240bc2bc1b78083e67f1e84e1b658
joeyjojo <joeyjojosmith@gmail.com>
2020-04-27 11:46:56 +0100

    Added collection component for displaying connections on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,70 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
+function CollectionIndex(props) {
+  const { collections } = props
+  return (
+    <div
+      style={{
+        marginLeft: "1vw",
+        marginRight: "1vw",
+      }}
+    >
+      <h2>Collections</h2>
+      <ul>
+        {collections.map(({ node }) => {
+          const title = node.frontmatter.title
+          return (
+            <li>
+              <h3>
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+function PostsIndex(props) {
+  const { posts } = props
+  return (
+    <div
+    style={{
+      marginLeft: "1vw",
+      marginRight: "1vw",
+    }}    
+    >
+      <h2>Posts</h2>
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <div key={node.fields.slug}>
+            <h3
+              style={{
+                marginBottom: rhythm(1 / 4),
+              }}
+            >
+              <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                {title}
+              </Link>
+            </h3>
+            <small>{node.frontmatter.date}</small>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: node.frontmatter.description || node.excerpt,
+              }}
+            />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
@@ -12,8 +76,9 @@ class BlogIndex extends React.Component {
     const posts = data.blogPosts.edges.filter(({ node }) => {
       return node.frontmatter.published
     })
-    //const collections = data.collections.edges;
-
+    const collections = data.collections.edges.filter(({ node }) => {
+      return node.frontmatter.published
+    })
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
@@ -22,31 +87,10 @@ class BlogIndex extends React.Component {
             display: "flex",
           }}
         >
-          <div>
-            <h2>Posts</h2>
-            {posts.map(({ node }) => {
-              const title = node.frontmatter.title || node.fields.slug
-              return (
-                <div key={node.fields.slug}>
-                  <h3
-                    style={{
-                      marginBottom: rhythm(1 / 4),
-                    }}
-                  >
-                    <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                      {title}
-                    </Link>
-                  </h3>
-                  <small>{node.frontmatter.date}</small>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: node.frontmatter.description || node.excerpt,
-                    }}
-                  />
-                </div>
-              )
-            })}
-          </div>
+          {collections.length > 0 && (
+            <CollectionIndex collections={collections} />
+          )}
+          {posts.length > 0 && <PostsIndex posts={posts} />}
         </div>
       </Layout>
     )
@@ -56,27 +100,46 @@ class BlogIndex extends React.Component {
 export default BlogIndex
 
 export const pageQuery = graphql`
-{
-  site {
-    siteMetadata {
-      title
+  {
+    site {
+      siteMetadata {
+        title
+      }
     }
-  }
-  blogPosts: allMarkdownRemark(filter: {fields: {sourceName: {eq: "blog"}}}, sort: {fields: [frontmatter___date], order: DESC}) {
-    edges {
-      node {
-        excerpt
-        fields {
-          slug
+    blogPosts: allMarkdownRemark(
+      filter: { fields: { sourceName: { eq: "blog" } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+            published
+          }
         }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-          published
+      }
+    }
+    collections: allMarkdownRemark(
+      filter: { fields: { sourceName: { eq: "collection" } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            published
+          }
         }
       }
     }
   }
-}
 `
